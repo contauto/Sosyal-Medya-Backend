ec8a88830a83fe359b48f799002de7d244eff2bd
contauto <controleng@yandex.com>
2022-09-10 14:31:26 +0300

    Spring Boot

--- src/main/java/com/example/learningSpring/user/User.java
+++ src/main/java/com/example/learningSpring/user/User.java
@@ -2,6 +2,8 @@ package com.example.learningSpring.user;
 import lombok.Data;
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @Data
 @Entity
@@ -11,12 +13,18 @@ public class User {
     @GeneratedValue
     private long id;
 
+
     @NotNull
+    @Size(min = 3,max = 48)
+    @UniqueUsername
     private String username;
 
+    @Size(min = 2,max = 64)
     @NotNull
     private String name;
 
+    @Size(min = 8,max = 64)
+    @Pattern(regexp="^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$")
     @NotNull
     private String password;
 }
--- src/main/java/com/example/learningSpring/user/UserRepository.java
+++ src/main/java/com/example/learningSpring/user/UserRepository.java
@@ -3,4 +3,7 @@ package com.example.learningSpring.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User,Long> {
+
+    User findByUsername(String username);
+
 }
